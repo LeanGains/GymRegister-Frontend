dc0e3bf0a009c8b48dc45d8188705f8bd06f293c
gh-leangains <hello@leangains.digital>
2025-09-06 17:04:32 +0100

    Create Equipment Scanner page

---
+++ src/pages/EquipmentScanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EquipmentScanner: React.FC = () => {
+  return (
+    <div>
+      <h1>Equipment Scanner</h1>
+    </div>
+  );
+};
+
+export default EquipmentScanner;
